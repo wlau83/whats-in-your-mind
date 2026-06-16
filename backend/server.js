4609cf7919c6756e0863549fb1236501c60064c8
wlau83 <workhkmh@gmail.com>
2026-06-16 17:49:11 -0400

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -31,9 +31,6 @@ app.get("/", (req, res) => {
 app.use("/api/auth", authRoutes);
 app.use("/api/thoughts", thoughtRoutes);
 
-app.get("/", (req, res) => {
-  res.send("What’s on your mind API is running");
-});
 
 const PORT = process.env.PORT || 5000;
 
